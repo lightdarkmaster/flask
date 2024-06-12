ed2b557e2f94d6271f8dab5a541cd3f00d13c3c4
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-06-12 18:17:30 +0800

    Make the flask framework debugable

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
-    #app.run(debug=True)
+    #app.run(host='0.0.0.0', port=8000)
+    app.run(debug=True)
